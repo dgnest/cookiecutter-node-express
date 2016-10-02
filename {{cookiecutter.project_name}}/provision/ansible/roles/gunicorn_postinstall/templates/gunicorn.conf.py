3565bb0d79391e5ae732e2702fbb2ddb5b951017
@slovacus <slovacus@gmail.com>
2016-10-02 14:55:39 -0500

    [#2] Fix: clean roles ansible unnecesary

--- {{cookiecutter.project_name}}/provision/ansible/roles/gunicorn_postinstall/templates/gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-"""Configuration Gunicorn."""
-import multiprocessing
-
-logfile = '{{ gunicorn.dir.logs }}/gunicorn.log'
-bind = '127.0.0.1:9000'
-max_requests = '{{ gunicorn.max_requests }}'
-# worker_class = 'gevent'
-workers = multiprocessing.cpu_count() * 2 + 1
